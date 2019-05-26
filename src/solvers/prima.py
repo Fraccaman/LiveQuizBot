3b04313a8ff8bc9395a5a827c49864eec36ec141
Gianmarco Fraccaroli <gianmarco.fraccaroli@gmail.com>
2019-05-26 11:33:44 +0200

    Error handling

--- src/solvers/prima.py
+++ src/solvers/prima.py
@@ -24,13 +24,16 @@ class Prima(Solver):
 
     def get_points_from_texts(self, html: str):
         soup = BeautifulSoup(html, features="html.parser")
-        date_str = soup.find('div', {'class': 'Z0LcW'}).text
-        d  = parse(date_str).timestamp()
-        return int(d)
-
-    def select_points(self, followers: Dict):
+        try:
+            date_str = soup.find('div', {'class': 'Z0LcW'}).text
+            d  = parse(date_str).timestamp()
+            return int(d)
+        except Exception:
+            return 0
+            
+    def select_points(self, dates: Dict):
         return {
-            self.copy.first_answer: followers[0],
-            self.copy.second_answer: followers[1],
-            self.copy.third_answer: followers[2]
+            self.copy.first_answer: dates[0],
+            self.copy.second_answer: dates[1],
+            self.copy.third_answer: dates[2]
         }
